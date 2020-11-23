8677884bc74c903412b18d1462e539e25996bb34
Jason Godmere <jgodmere@UX550VE-laptop.attlocal.net>
2020-11-22 19:19:37 -0500

    Finished basic database structure for Clusters, Nodes, Users and Profiles

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Cluster
+from .models import User, Profile, Cluster, Node
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Profile)
 
 
 class ClusterAdmin(admin.ModelAdmin):
     list_display = ('name', 'owner', 'created_on', 'last_update')
 
 admin.site.register(Cluster, ClusterAdmin)
+
+
+class NodeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'ip_address', 'cluster', 'initialized', 'last_update')
+
+admin.site.register(Node, NodeAdmin)
--- core/models.py
+++ core/models.py
@@ -1,11 +1,22 @@
 
 from django.db import models
 
-# Custom user model
+# Custom user authentication model
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
 	pass
+
+# Profile is just an extension of the user (one to one)
+class Profile(models.Model):
+	user = models.OneToOneField(
+		User,
+		on_delete=models.CASCADE,
+		primary_key=True,
+	)
+
+	def __str__(self):
+		return self.user.username
 
 
 
@@ -15,10 +26,30 @@ class Cluster(models.Model):
 	name = models.CharField(max_length=100, unique=True)
 	created_on = models.DateTimeField(auto_now_add=True)
 	last_update = models.DateTimeField(auto_now=True)
+
 	owner = models.ForeignKey(User, on_delete=models.CASCADE)
+	contributors = models.ManyToManyField('Profile', blank=True, null=True)
+
 
 	def __str__(self):
 		return self.name
 
 	class Meta:
 		ordering = ['name']
+
+
+
+# EOH Node Model
+# only registers new nodes into database if the owner has initialized it into a cluster
+class Node(models.Model):
+	ip_address = models.GenericIPAddressField()
+	name = models.CharField(max_length=100)
+	initialized = models.DateTimeField(auto_now_add=True)
+	last_update = models.DateTimeField(auto_now=True)
+	cluster = models.ForeignKey(Cluster, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.ip_address
+
+	class Meta:
+		ordering = ['ip_address']
